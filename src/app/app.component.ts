d7e3db5db33b631df7bbbcb88210a001e0804d45
Gregor Woiwode <gw@co-it.eu>
2016-09-21 14:50:55 +0200

    Uses VocableService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VocablesService } from './services';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private vocables: VocablesService) {
+      this.vocables.useApiGateways();
+  }
 }
