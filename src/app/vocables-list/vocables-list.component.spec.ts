d9bfae3abecee661dec30ade5a76889124d94b34
Gregor Woiwode <gw@co-it.eu>
2016-09-21 14:49:59 +0200

    Adds component listing list of vocable

---
+++ src/app/vocables-list/vocables-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { VocablesListComponent } from './vocables-list.component';
+
+describe('Component: VocablesList', () => {
+  it('should create an instance', () => {
+    let component = new VocablesListComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/vocables-list/vocables-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vocables-list',
+  templateUrl: './vocables-list.component.html',
+  styleUrls: ['./vocables-list.component.css']
+})
+export class VocablesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
